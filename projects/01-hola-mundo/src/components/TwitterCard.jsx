8540e4babd1fa9875b8e1221a8efb05ef3786aca
Fercho5656 <morenomoranfernando@gmail.com>
2023-01-22 22:35:43 -0600

    Add internal state

--- projects/01-hola-mundo/src/components/TwitterCard.jsx
+++ projects/01-hola-mundo/src/components/TwitterCard.jsx
@@ -1,17 +1,24 @@
 import './TwitterCard.css'
+import { useState } from 'react'
 
 export default function TwitterCard() {
+  const [isFollowing, setIsFollowing] = useState(false)
+  const onFollow = () => setIsFollowing(!isFollowing)
+
+  const className = isFollowing ? 'tw-followCard-followBtn following' : 'tw-followCard-followBtn'
+  const followingText = isFollowing ? 'Following' : 'Follow'
+
   return (
     <a href="#" className="tw-followCard">
       <img className="tw-followCard-avatar" src="https://unavatar.io/midudev" alt="Profile Picture" />
-        <div className="tw-followCard-text">
-          <strong className="tw-followCard-text-fullname">Miguel Ángel Durán</strong>
-          <span className="tw-followCard-text-username">@midudev</span>
-        </div>
-        <button className="tw-followCard-followBtn">
-          <span className="text-follow">Follow</span>
-          <span className="text-unfollow">Unfollow</span>
-        </button>
+      <div className="tw-followCard-text">
+        <strong className="tw-followCard-text-fullname">Miguel Ángel Durán</strong>
+        <span className="tw-followCard-text-username">@midudev</span>
+      </div>
+      <button onClick={onFollow} className={className}>
+        <span className="text-follow">{followingText}</span>
+        <span className="text-unfollow">Unfollow</span>
+      </button>
     </a>
   )
 }
