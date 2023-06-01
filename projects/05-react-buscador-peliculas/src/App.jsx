cac1cff67f04dfcb66cc34a49fc400285025c1e2
Fernando Moreno <morenomoranfernando@gmail.com>
2023-06-01 14:10:15 -0600

    Restore project

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,49 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-import { useEffect, useState } from 'react'
-import withResults from './mock/withResults.json'
-import noResults from './mock/noResults.json'
-import { Movies } from './components/Movies'
-import { getMovies } from './services/movies'
-
-function App () {
-  const [movies, setMovies] = useState([])
-  const [search, setSearch] = useState('')
-
-  useEffect(() => {
-    getMovies(search)
-      .then(movies => {
-        setMovies(movies)
-      })
-      .catch(err => {
-        console.log(err)
-        setMovies([])
-      })
-  }, [search])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const { movieName } = e.target
-    console.log(movieName.value)
-    if (movieName.value === '') return setMovies([])
-    const movies = await getMovies(movieName.value)
-    console.log(movies)
-    // setMovies(withResults.Search)
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <main>
-      <h1>Buscador de Peliculas</h1>
-      <form onSubmit={handleSubmit}>
-        <input name='movieName' placeholder='John Wick' />
-        <button>Buscar</button>
-      </form>
-      <section>
-        {movies.length > 0
-          ? <Movies movies={movies} />
-          : <p>Pelicula no encontrada</p>}
-      </section>
-    </main>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
